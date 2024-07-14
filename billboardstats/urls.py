51dc95144c29d3ba45c8968d17c1ba173ab2702c
David Mays <maysidavid@gmail.com>
2024-07-13 21:35:30 -0500

    Fleshed out initial templates for bio and billboardstats

--- billboardstats/urls.py
+++ billboardstats/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'billboardstats'
 
 urlpatterns = [
-    path('', views.chart, name='home'),
+    path('', views.search, name='home'),
     path('chart', views.chart, name='chart'),
     path('song', views.song, name='song'),
     path('artist', views.artist, name='artist'),
--- billboardstats/views.py
+++ billboardstats/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+# Search
+def search(request):
+    return render(request, 'billboardstats/search.html')
+
 # Chart
 def chart(request):
     return render(request, 'billboardstats/chart.html')
