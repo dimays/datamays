d27ae815f5f27d503e1391b539a0d1118e6c37c8
David Mays <maysidavid@gmail.com>
2026-01-06 02:00:45 +0000

    Add Contact form with Google SMTP

---
+++ contact/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+
+# No models to register.
+# This app is intentionally form + email only.
---
+++ contact/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+# No models to create.
+# This app is intentionally form + email only.
